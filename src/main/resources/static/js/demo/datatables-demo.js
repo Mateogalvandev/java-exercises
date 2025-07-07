c5fa014fefed8e87f01cc3db1e5561992c82ca1a
Mateogalvandev <matedev0612@gmail.com>
2025-07-07 11:30:24 -0300

    Base sin ningun codigo, recien creado el proyecto con sus respectivos repositorios, unicamente se le añadio sus archivos estaticos y las plantillas thymeleaf

---
+++ src/main/resources/static/js/demo/datatables-demo.js
@@ -0,0 +1,21 @@
+ $(document).ready(function() {
+        $('#dataTable').DataTable({
+            "columnDefs": [
+                { "orderable": false, "targets": 4 } // Deshabilita ordenamiento para la 5ta columna (índice 4)
+            ],
+            "language": {
+                "lengthMenu": "Mostrar _MENU_ registros por página",
+                "zeroRecords": "No se encontraron resultados",
+                "info": "Mostrando _START_ a _END_ de _TOTAL_ registros",
+                "infoEmpty": "Mostrando 0 a 0 de 0 registros",
+                "infoFiltered": "(filtrado de _MAX_ registros totales)",
+                "search": "Buscar:",
+                "paginate": {
+                    "first": "Primero",
+                    "last": "Último",
+                    "next": "Siguiente",
+                    "previous": "Anterior"
+                }
+            }
+        });
+    });
